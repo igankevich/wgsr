443f8cbe00ef5cdf63230f13e85cdb2c844bd79f
Ivan Gankevich <github@capybaramail.xyz>
2024-08-16 08:32:36 +0200

    Wgproto

--- ci/binaries.sh
+++ ci/binaries.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 . ./ci/preamble.sh
+export RUSTFLAGS="-Ccodegen-units=1 -Copt-level=z -Cincremental=false -Clto=yes -Cembed-bitcode=yes -Cstrip=symbols"
+cargo build \
+    --release \
+    --target x86_64-unknown-linux-musl \
+    --no-default-features
