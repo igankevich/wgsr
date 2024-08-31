842389130c7e865c43743ec07bb738a416bb38b7
Ivan Gankevich <github@capybaramail.xyz>
2024-08-31 08:19:58 +0200

    Extract `Endpoint`.

--- src/bin/wgx-hub/main.rs
+++ src/bin/wgx-hub/main.rs
@@ -1,9 +1,6 @@
 use std::collections::HashSet;
-use std::net::IpAddr;
 use std::net::SocketAddr;
-use std::net::ToSocketAddrs;
 use std::process::ExitCode;
-use std::str::FromStr;
 
 use clap::Parser;
 use clap::Subcommand;
@@ -12,10 +9,12 @@ use wgx::FromBase64;
 use wgx::ToBase64;
 use wgx::DEFAULT_LISTEN_PORT;
 
+use self::endpoint::*;
 use self::wg::*;
 use self::wgx_client::*;
 use crate::get_relay_ip_addr_and_peers_public_keys;
 
+mod endpoint;
 mod wg;
 mod wgx_client;
 
@@ -129,56 +128,5 @@ fn do_main() -> Result<ExitCode, Box<dyn std::error::Error>> {
             Ok(ExitCode::SUCCESS)
         }
         None => Ok(ExitCode::SUCCESS),
-    }
-}
-
-enum Endpoint {
-    SocketAddr(SocketAddr),
-    IpAddr(IpAddr),
-    DnsNameWithPort(DnsNameWithPort),
-    DnsName(String),
-}
-
-impl Endpoint {
-    fn to_socket_addr(
-        &self,
-        default_port: u16,
-    ) -> Result<Option<SocketAddr>, Box<dyn std::error::Error>> {
-        match self {
-            Self::SocketAddr(x) => Ok(x.to_socket_addrs()?.next()),
-            Self::IpAddr(x) => Ok(Some(SocketAddr::new(*x, default_port))),
-            Self::DnsNameWithPort(x) => Ok(x.name.to_socket_addrs()?.next()),
-            Self::DnsName(x) => Ok(format!("{}:{}", x, default_port).to_socket_addrs()?.next()),
-        }
-    }
-}
-
-impl FromStr for Endpoint {
-    type Err = Box<dyn std::error::Error>;
-    fn from_str(other: &str) -> Result<Self, Self::Err> {
-        if let Ok(socket_addr) = other.parse::<SocketAddr>() {
-            return Ok(Self::SocketAddr(socket_addr));
-        }
-        if let Ok(ipaddr) = other.parse::<IpAddr>() {
-            return Ok(Self::IpAddr(ipaddr));
-        }
-        if let Ok(dns_name_with_port) = other.parse::<DnsNameWithPort>() {
-            return Ok(Self::DnsNameWithPort(dns_name_with_port));
-        }
-        Ok(Self::DnsName(other.into()))
-    }
-}
-
-struct DnsNameWithPort {
-    name: String,
-}
-
-impl FromStr for DnsNameWithPort {
-    type Err = Box<dyn std::error::Error>;
-    fn from_str(other: &str) -> Result<Self, Self::Err> {
-        match other.rfind(':') {
-            Some(_) => Ok(Self { name: other.into() }),
-            None => Err(format!("no port in `{}`", other).into()),
-        }
     }
 }
